6443a1a6c119f73a8796fc127ff1d22de8c42f2d
tayrotayro <rotolotaylor@gmail.com>
2021-01-27 21:06:28 -0600

    delete

---
+++ routes/testAPI.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+router.get("/api/hey", function (req, res) {
+  res.send("API is working properly");
+});
+
+router.post("/api/mult", body("value").isNumeric(), (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.send({
+      error: "Please enter a valid number!",
+    });
+  }
+  const value = req.body.value;
+  //   body("value").isNumeric();
+  const value1 = value * 2;
+  res.send({
+    message: "hello",
+    data: value1,
+  });
+});
+
+module.exports = router;
